9d7e40725793cdf4b57f6a5795de44b60ba62c39
Sebastian Schuberth <sebastian.schuberth@bosch.io>
2020-03-20 15:59:39 +0100

    WIP

---
+++ detekt-rules/src/main/kotlin/OrtImportOrder.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2020 Bosch.IO GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package com.here.ort.detekt
+
+import io.gitlab.arturbosch.detekt.api.Debt
+import io.gitlab.arturbosch.detekt.api.Issue
+import io.gitlab.arturbosch.detekt.api.Rule
+import io.gitlab.arturbosch.detekt.api.Severity
+import io.gitlab.arturbosch.detekt.api.internal.absolutePath
+
+//import org.jetbrains.kotlin.psi.KtElement
+import org.jetbrains.kotlin.psi.KtImportList
+import org.jetbrains.kotlin.psi.psiUtil.siblings
+
+class OrtImportOrder : Rule() {
+    override val issue = Issue(
+        javaClass.simpleName,
+        Severity.Style,
+        "Reports files that do not follow ORT's order for imports",
+        Debt.FIVE_MINS
+    )
+
+    override fun visitImportList(importList: KtImportList) {
+        super.visitImportList(importList)
+
+        //val ktElement = importList.siblings(withItself = false).filterIsInstance<KtElement>().firstOrNull() ?: return
+        //println(importList.siblings(withItself = false).joinToString())
+        val sortedImports = importList.imports.sortedBy { it.importPath.toString() }
+        if (importList.imports != sortedImports) {
+            println(importList.containingKtFile.absolutePath())
+            println(importList.imports.joinToString { it.importPath.toString() })
+            println(sortedImports.joinToString { it.importPath.toString() })
+        }
+    }
+}
